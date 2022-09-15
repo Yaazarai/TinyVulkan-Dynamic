f7a1fd0e3071d59ce277512c8960fbcd992bf387
Alexander Griffis <fatalsleep@gmail.com>
2022-09-14 17:31:07 -0700

    Re-Add project for gitignore reset.

---
+++ MiniVulkan/MiniVkCommandPool.hpp
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef MINIVK_MINIVKCOMMANDPOOL
+#define MINIVK_MINIVKCOMMANDPOOL
+	#include "./MiniVk.hpp"
+
+	namespace MINIVULKAN_NS {
+		class MiniVkCommandPool : public MiniVkObject {
+		private:
+			MiniVkInstanceSupportDetails mvkLayer;
+			VkCommandPool commandPool;
+			std::vector<VkCommandBuffer> commandBuffers;
+		public:
+			void Disposable() {
+				vkDeviceWaitIdle(mvkLayer.logicalDevice);
+				vkDestroyCommandPool(mvkLayer.logicalDevice, commandPool, nullptr);
+			}
+			
+			MiniVkCommandPool(MiniVkInstanceSupportDetails mvkLayer, size_t bufferCount = 1) : mvkLayer(mvkLayer) {
+				onDispose += std::callback<>(this, &MiniVkCommandPool::Disposable);
+				CreateCommandPool();
+				CreateCommandBuffers(bufferCount+1);
+			}
+
+			MiniVkCommandPool operator=(const MiniVkCommandPool& cmdPool) = delete;
+
+			VkCommandPool& GetPool() { return commandPool; }
+			std::vector<VkCommandBuffer>& GetBuffers() { return commandBuffers; }
+			size_t GetBufferCount() { return commandBuffers.size(); }
+
+			void CreateCommandPool() {
+				VkCommandPoolCreateInfo poolInfo{};
+				poolInfo.sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO;
+				poolInfo.flags = VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT;
+				poolInfo.queueFamilyIndex = MiniVkQueueFamily::FindQueueFamilies(
+					mvkLayer.physicalDevice, mvkLayer.window->GetWindowSurface()
+				).graphicsFamily.value();
+
+				if (vkCreateCommandPool(mvkLayer.logicalDevice, &poolInfo, nullptr, &commandPool) != VK_SUCCESS)
+					throw std::runtime_error("MiniVulkan: Failed to create command pool!");
+			}
+
+			void CreateCommandBuffers(size_t bufferCount = 1) {
+				commandBuffers.resize(bufferCount);
+
+				VkCommandBufferAllocateInfo allocInfo{};
+				allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+				allocInfo.commandPool = commandPool;
+				allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+				allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+				if (vkAllocateCommandBuffers(mvkLayer.logicalDevice, &allocInfo, commandBuffers.data()) != VK_SUCCESS)
+					throw std::runtime_error("MiniVulkan: Failed to allocate command buffers!");
+			}
+		};
+	}
+#endif
---
+++ MiniVulkan/MiniVkThreadPool.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#ifndef MINIVK_MINIVKTHREADPOOL
+#define MINIVK_MINIVKTHREADPOOL
+	#include "./MiniVk.hpp"
+
+	namespace MINIVULKAN_NS {
+		class MiniVkThreadPool : MiniVkObject {
+		private:
+			void slave() {
+				while (working) {
+					if (safety_lock.try_lock()) {
+						std::callback<std::atomic_bool&> callback = queue.front();
+						queue.pop();
+						safety_lock.unlock();
+						callback.invoke(working);
+					} else { std::this_thread::yield(); }
+				}
+			}
+		public:
+			std::mutex safety_lock;
+			std::atomic_bool working;
+			std::queue<std::callback<std::atomic_bool&>> queue;
+			std::vector<std::thread> pool;
+
+			MiniVkThreadPool(const uint32_t potentialThreads = 2, bool startWorking = true) : working(startWorking) {
+				onDispose += std::callback<>(this, &MiniVkThreadPool::awaitClosePool);
+				trySpawnThreads(potentialThreads, startWorking);
+			}
+
+			size_t getSize() { return pool.size(); }
+
+			void clearTasks() { std::queue<std::callback<std::atomic_bool&>>().swap(queue); }
+
+			void awaitClosePool() {
+				working = false;
+				while (pool.size() > 0)
+					for (std::thread& t : pool)
+						if (t.joinable()) {
+							t.join();
+							std::erase_if(pool, [&](const std::thread& tt) { return tt.get_id() == t.get_id(); });
+						}
+				pool.clear();
+			}
+
+			size_t trySpawnThreads(const uint32_t potentialThreads = 2, bool startWorking = false) {
+				size_t threadCount = 0;
+				try {
+					const size_t hw = std::thread::hardware_concurrency();
+					const size_t threads = (hw == 0) ? potentialThreads : hw;
+					threadCount = threads - pool.size();
+
+					for (size_t t = 0; t < threadCount; t++)
+						pool.push_back(std::thread(&MiniVkThreadPool::slave, this));
+				} catch (...) { working = false; throw; }
+				return threadCount;
+			}
+
+			bool tryPushCallback(std::callback<std::atomic_bool&> task) {
+				if (safety_lock.try_lock()) {
+					queue.push(task);
+					safety_lock.unlock();
+					return true;
+				}
+				return false;
+			}
+		};
+	}
+#endif
